50f264b940972fc4a620a8ba1988a5911e4081e2
Senk111 <akash.baral111@gmail.com>
2023-12-16 11:44:33 +0530

    1st Push

---
+++ Main1Jenkins/src/test/java/main1/CreateContactTest.java
@@ -0,0 +1,14 @@
+package main1;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest 
+{
+	@Test
+	public void createContactTest() 
+	{
+		System.out.println("9999000099");
+		
+	}
+
+}
---
+++ Main1Jenkins/src/test/java/main1/CreateEmailTest.java
@@ -0,0 +1,14 @@
+package main1;
+
+import org.testng.annotations.Test;
+
+public class CreateEmailTest 
+{
+	@Test
+	public void createEmailTest() 
+	{
+		System.out.println("zumba@tmail.com");
+		
+	}
+
+}
